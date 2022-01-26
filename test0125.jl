13a2eafea2a140095306365928ce40589cc0e2b2
takahiro <takazone108@gmail.com>
2022-01-26 16:34:52 +0900

    tuple funtction

--- test0125.jl
+++ test0125.jl
@@ -1,16 +1,21 @@
 using Distributed
+rmprocs(workers())
 addprocs(1)
 @everywhere function plus(x)
-    return(x + 1)
+    return(x + 1,x + 2)
 end
 
 function main()
     @distributed for i in 1:3
         open("./ditest.dat","a+") do f
-            fi = plus(i)
-            println(f,"$(i) $(fi)")            
+            fi,gi = plus(i)
+            
+            println(f,"$(i) $(fi) $(gi)")            
         end
     end
 end
 
 @time main()
+
+
+rmprocs(workers())
